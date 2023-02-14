81b89c712c162ebcd1c974a01cb4187cd5f92b3e
OM KUMAR PRIY <omkumarpriy@gmail.com>
2023-02-14 17:39:03 +0530

    Acio #rd Assig

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly obj=new RWOnly();
+       // obj.name="om";
+        // it give error /s name is private variable
+        obj.setName("Om");
+        System.out.println(obj.getName());
+    }
   
 }
